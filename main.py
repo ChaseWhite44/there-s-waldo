c66fbbe078f859a98381706aa2980cd85842ed62
ChaseWhite44 <60238377+ChaseWhite44@users.noreply.github.com>
2020-12-17 04:09:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from TemplateMatch import *
 import cv2
 import os
+import matplotlib.pyplot as plt
 from waldo import color
 
 dir = "images"
@@ -17,8 +18,18 @@ def main(f,f1):
     image = cv2.imread(dir + "/" + f[i])
     template = cv2.imread(dir1 + "/" + f1[i])
 
-    templateMatching(image, template)
-    color(dir + "/" + f[i])
+    seg = color(dir + "/" + f[i])
+    box = templateMatching(image, template)
+    
+    fig = plt.figure()
+fig.add_subplot(4, 1, 1)
+imgplot = plt.imshow(seg)
+fig.add_subplot(4, 1, 2)
+imgplot = plt.imshow(image)
+fig.add_subplot(4, 1, 3)
+imgplot = plt.imshow(template)
+fig.add_subplot(4, 1, 4)
+imgplot = plt.imshow(box)
 
 if __name__ == '__main__':
     main(files,files1)
